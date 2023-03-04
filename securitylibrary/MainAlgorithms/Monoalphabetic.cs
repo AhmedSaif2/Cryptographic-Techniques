2e2a706aa1ab11962fe2f28d9699cb4f3c33ffda
Ahmed Saif <ahmed.saif@bld.ai>
2023-03-04 23:27:53 +0200

    add analyse by char frequency

--- securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -53,47 +53,55 @@ namespace SecurityLibrary
                 cipherText += key[charNumber];
             }
             return cipherText;
-        }         
+        }
         /// <summary>
-                            /// Frequency Information:
-                            /// E   12.51%
-                            /// T    9.25
-                            /// A    8.04
-                            /// O    7.60
-                            /// I    7.26
-                            /// N    7.09
-                            /// S    6.54
-                            /// R    6.12
-                            /// H    5.49
-                            /// L    4.14
-                            /// D    3.99
-                            /// C    3.06
-                            /// U    2.71
-                            /// M    2.53
-                            /// F    2.30
-                            /// P    2.00
-                            /// G    1.96
-                            /// W    1.92
-                            /// Y    1.73
-                            /// B    1.54
-                            /// V    0.99
-                            /// K    0.67
-                            /// X    0.19
-                            /// J    0.16
-                            /// Q    0.11
-                            /// Z    0.09
-                            /// </summary>
-                            /// <param name="cipher"></param>
-                            /// <returns>Plain text</returns>
+                  /// Frequency Information:
+                  /// E   12.51%
+                  /// T    9.25
+                  /// A    8.04
+                  /// O    7.60
+                  /// I    7.26
+                  /// N    7.09
+                  /// S    6.54
+                  /// R    6.12
+                  /// H    5.49
+                  /// L    4.14
+                  /// D    3.99
+                  /// C    3.06
+                  /// U    2.71
+                  /// M    2.53
+                  /// F    2.30
+                  /// P    2.00
+                  /// G    1.96
+                  /// W    1.92
+                  /// Y    1.73
+                  /// B    1.54
+                  /// V    0.99
+                  /// K    0.67
+                  /// X    0.19
+                  /// J    0.16
+                  /// Q    0.11
+                  /// Z    0.09
+                  /// </summary>
+                  /// <param name="cipher"></param>
+                  /// <returns>Plain text</returns>
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            //Tuple<char, int>[] cipher_freq = new Tuple<char, int>[26];
-
-            //string mostfreq = "etaoinsrhldcumfpgwybvkxjqz";
-            //for (int i = 0; i < cipher.Length; i++)
-            //    cipher_freq[cipher[i] - 'a'].Item1 = cipher[i];
-            return "";
-
+            Dictionary<char, int> cipher_freq = new Dictionary<char, int>();
+            string mostfreq = "etaoinsrhldcumfpgwybvkxjqz";
+            cipher = cipher.ToLower();
+            for (int i = 0; i < 26; i++)
+                cipher_freq.Add((char)('a' + i), 0);
+            for (int i = 0; i < cipher.Length; i++)
+                cipher_freq[cipher[i]]++;
+            cipher_freq = cipher_freq.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            char[] key = new char[26];
+            int back = 25;
+            foreach (var item in cipher_freq)
+            {
+                key[mostfreq[back--] - 'a'] = item.Key;
+            }
+            return Decrypt(cipher.ToUpper(),new string(key));
         }
         public HashSet<char> prepareCharacterSet()
         {
